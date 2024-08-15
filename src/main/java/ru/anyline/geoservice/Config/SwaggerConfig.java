84acc84c52224468f21741ae4142fb11654115e8
anyline <mymail@anyline.no>
2024-08-15 11:55:32 +0300

    api docs+

---
+++ src/main/java/ru/anyline/geoservice/Config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package ru.anyline.geoservice.Config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Geocoding service")
+                        .version("15.08")
+                        .description("API для кастомной URL"));
+    }
+}
